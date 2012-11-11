a1631c1c6cc1c2fc1af590c81394f4ea8fc4f761
Kevin Urrutia <urrutia.kevin@gmail.com>
2012-11-10 23:43:40 -0800

    dfd

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,23 +21,23 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      run "/etc/init.d/unicorn_#{application} #{command} "
     end
   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    #put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
+  task :cold do
+    deploy.update
+    deploy.start
   end
-  after "deploy:setup", "deploy:setup_config"
 
-  # task :symlink_config, roles: :app do
-  #   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  # task :setup_config, roles: :app do
+  #   sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+  #   sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+  #   run "mkdir -p #{shared_path}/config"
+  #   #put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+  #   puts "Now edit the config files in #{shared_path}."
   # end
-  # after "deploy:finalize_update", "deploy:symlink_config"
+  # after "deploy:setup", "deploy:setup_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
