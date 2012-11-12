6035d4aaa9521663a6a25ec5243809fa4be94c44
Kevin Urrutia <urrutia.kevin@gmail.com>
2012-11-11 22:52:03 -0800

    removed new relic

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ require 'sinatra/static_assets'
 require 'will_paginate'
 require 'will_paginate/data_mapper'  # or data_mapper/sequel
 
-
 DataMapper.setup(:default, "postgres://snicksnaps:secret@localhost/snicksnaps_production" || "sqlite:///#{Dir.pwd}/pinn.db")
 #DataMapper.setup(:default, "sqlite:///#{Dir.pwd}/pinn.db" || "postgres://snicksnaps:secret@localhost/snicksnaps_production")
 
--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
 require './app'
-require 'newrelic_rpm'
 run Sinatra::Application
